fa4ac46429dba35772b047ac8c14220d91f8db87
Andresfgomez970 <andresf.gomez970@gmail.com>
2020-02-03 12:57:02 -0500

    docs: end to comment the code an restructure to make tests in testing

--- model.py
+++ model.py
@@ -32,7 +32,7 @@ class BasicModel(nn.Module):
         # Create the layers of the LSTM one by one, this is not done with
         #   stack long short term memory nn package because this one only
         #   permits same size for the middle layers; which is not the case
-        #   for other works; note that for each we give a different name.
+        #   for works as this one; note that for each we give a different name.
         for i in range(self.n_layer):
             self.__setattr__('lstm%d' % i,
                              nn.LSTM(last_output_size, layer_sizes[i]))
@@ -41,7 +41,8 @@ class BasicModel(nn.Module):
     def init_hidden(self, batch_size, to_cuda=False):
         """
         This routine initialize the the hiddden states of all the stacked
-        layers, remember stack laters communicate with one another.
+        layers, remember each stack layers communicate with itself in the next
+        recurrent step an with the other in the same recurrent step.
 
         batch_size(int): number of examples.
         to_cuda(bool): indication to make the objects in cuda in order to
@@ -51,8 +52,9 @@ class BasicModel(nn.Module):
 
         for i in range(self.n_layer):
             # Init made for each hidden layer according to N(0,1), in oder
-            #  for better backprop behaviour, the first parameter is one
-            #  beacuse stacked layers are not desired to have the same dim.
+            #  for better backprop behaviour.
+            # The first parameter is one beacuse the stack initialization is
+            #  made for each latyer indiviaully.
             hidden_state = torch.randn(1, batch_size, self.layer_sizes[i])
 
             # If cuda paralelization option is on, then use it.
@@ -67,10 +69,10 @@ class BasicModel(nn.Module):
     def forward(self, x, to_cuda=False):
         """
         This method implements the general forward for a stack lstm neuronal
-          network with the different layers already defined.
+          network; with the different layers already defined.
         """
         # Remmber that: x.dim = (n_seq, batch_size, input_size). In standart
-        #  notation they are writen h and c respectively.
+        #  notation the next lines are writen h and c respectively.
         self.hidden = self.init_hidden(x.shape[1], to_cuda)
         self.state = self.init_hidden(x.shape[1], to_cuda)
 
@@ -79,7 +81,8 @@ class BasicModel(nn.Module):
 
         # Do forward calculations passing through every lstm cell; thanks to
         #  the nn library each lstm1, ..., lstmN connect to itself in the other
-        #  step.
+        #  step; the gradient descent will be well mada thanks to the fact that
+        #  calculations process is save to then apply autograd.
         for i in range(self.n_layer):
             lstm = self.__getattr__('lstm%d' % i)
 
@@ -93,9 +96,7 @@ class TimeModel(BasicModel):
     This is a clear copy of BasicModel and it is done to make everyting more
       clear for the Music_generation project.
     """
-
-    def __init__(self, input_size, layer_sizes):
-        super(TimeModel, self).__init__(input_size, layer_sizes)
+    pass
 
 
 class PitchModel(BasicModel):
@@ -103,8 +104,8 @@ class PitchModel(BasicModel):
     This is almost a copy of BasicModel, in addition it makes the following:
       1. An additional layer is add to the lstm with two units in order to
         modelate this as the probability of the input note to be hold or
-        articulated; the input note is given to TimeModel, this will be clearer
-        in Model.
+        articulated; the input note is not given derectly to PitchModel, this
+        will be clearer in BiaxialRNNModel.
     """
 
     def __init__(self, input_size, layer_sizes):
@@ -127,26 +128,27 @@ class BiaxialRNNModel(nn.Module):
     This is designed to contain the implmentation inspired in the repo
       https://github.com/hexahedria/biaxial-rnn-music-composition and its
       respective blog where is explained with some degree of detail.
-    The folloing lines try to summary the process meake with the data during
+    The folloing lines try to summary the process meade with the data during
       the network.
 
     1. Data that was extracted from midi file songs in a tensorial form is
       given as input, for more details see the commented code midi_to_state_ma-
       trix.py.
-    2. The algorithm runs a RNN over time dimension after collapsing the note
-      dimensions and batch_size into one; this net is desired to discover all
-      possible time connections between the data in an ouput hidden_state.
+    2. The algorithm runs a stack LSTM RNN over time dimension after collapsing
+      the note  dimensions and batch_size into one; this net is desired to
+      discover all possible time connections between the data in an ouput
+      hidden_state.
     3. After this, the hidden_state vector of features is resize in order to
       contain additional info and data is reshaped in order to run a stacked
       lstm RNN over the notes; time and batch_size are collapsed in one
       dimension.
       Also in the each note step a two dimensional ouput is modelated in order
-      to simulate the porbability of the input note (in 1) to be hold and
-      articulated; note that thanks to the fact that as this depend of having
-      the correct ouput on the last one, this oput probabilities are really
-      conditional ones; and so the cost function is moedelated as the log
-      likelihood. For more details about this see https://www.youtube.com/watch
-      ?v=FHRQ2DhQjRM&list=PLZnyIsit9AM7yeTZuBmezKNc6hFHUPImh&index=6
+      to simulate the probability of the input note (in 1. above) to be hold
+      and articulated; note that thanks to the fact that as this depend of
+      having the correct ouput on the last one, this oput probabilities are
+      really conditional ones; and so the cost function is moedelated as the
+      log likelihood. For more details about this see https://www.youtube.com/
+      watch?v=FHRQ2DhQjRM&list=PLZnyIsit9AM7yeTZuBmezKNc6hFHUPImh&index=6
     """
 
     def __init__(self, t_layer_sizes, p_layer_sizes):
@@ -159,7 +161,7 @@ class BiaxialRNNModel(nn.Module):
 
         # The dimension of the last layer size is increase by two, to include
         #  information of the last note in each note step, this one says if
-        #  the last note was hold and played
+        #  the last note was hold and played.
         self.pitch_model = PitchModel(self.time_model.layer_sizes[-1] + 2,
                                       p_layer_sizes)
 
@@ -167,81 +169,79 @@ class BiaxialRNNModel(nn.Module):
         """
         This forward is basicaly the implmentation on detail of what was said
           in te description of the class.
-        x(numpy_array): Input information of the notes played in different
+
+        x(torch.Tensor): Input information of the notes played in different
           songs, note that x.dim = ... complemented
         """
+
+        # output_mat is defined by the function midiToNoteStateMatrix in the
+        #  file midi_to_state_matrix; while input_mat has the last dimension
+        #  changed, with other information that talk about the pitch, real
+        #  frequency of the note, its sorrounding notes, the part that it
+        #  occupies in the bar and other info.
         input_mat, output_mat = x
 
+        # The last info in time dimension is eliminated, as each data in the
+        #   matrix (time, batch, number_notes, features) try to predic the next
+        #   one, that is to say, time=0 predicts time=1, then, time=1 predicts
+        #   time=2... and timed=n_time-2 predicts time=n_time-1; output that
+        #   is used later to compare with the knwon notes in order to train the
+        #   NN.
         input_slice = input_mat[:, 0:-1]
+        # Update dimensions.
         n_batch, n_time, n_note, n_ipn = input_slice.shape
 
+        # Permute and collapse the dimensions with n_time first to run the
+        #   RNN to find ouputs that contain the time connections.
         time_inputs = input_slice.permute((1, 0, 2, 3))
         time_inputs = time_inputs.reshape((n_time, n_batch * n_note, n_ipn))
 
+        # Run time_model
         last_output = self.time_model(time_inputs)
 
+        # Get the n_hidden; number of the features that contain the time
+        #  connections.
         n_hidden = self.time_model.layer_sizes[-1]
 
+        # Collapse data in order to run a RNN over the notes.
         last_output = last_output.reshape(
             (n_time, n_batch, n_note, n_hidden)).permute(
             (2, 1, 0, 3)).reshape((n_note, n_batch * n_time, n_hidden))
 
+        # The last note try to modelate the next, and for the first note
+        #  the only thing that we have is silence, then a silence note
+        #  is added.
         start_note_values = torch.zeros(1, last_output.shape[1], 2)
 
+        # This is the matrix that represents the notes to be predicted. The
+        #   last dim in note is erased again thanks to the fact that that this
+        #   does not predict a next one that can be compared; the firs time is
+        #   also erased because there is not first time note to predcit it, in
+        #   order to compare.
         correct_choices = output_mat[:, 1:, 0:-1, :].permute(
             (2, 0, 1, 3)).reshape((n_note - 1, n_batch * n_time, 2))
 
+        # Concate the silence that we have mentioned.
         note_choices_inputs = torch.cat([start_note_values, correct_choices],
                                         dim=0)
-
+        # We add to  the hidden feature that contain the time information
+        #  another two components that say if taht note was played in the
+        #  last one.
         note_inputs = torch.cat([last_output, note_choices_inputs], dim=2)
 
+        # Apply the stacked LTSM RNN trough note sequence.
         last_output = self.pitch_model(note_inputs)
 
+        # Save the expectation of the hold and play probabilities for the
+        #   notes.
         last_output = last_output.reshape(
             (n_note, n_batch, n_time, 2)).permute(1, 2, 0, 3)
 
         return last_output
 
 
-def test_time_model():
-    time_model = TimeModel(80, [300, 300])
-    print(time_model)
-
-    data = torch.randn(127, 780, 80)
-
-    out = time_model(data)
-
-    print(out.shape)
-
-
-def test_pitch_model():
-    pitch_model = PitchModel(80, [300, 300])
-    print(pitch_model)
-
-    data = torch.randn(127, 780, 80)
-
-    out = pitch_model(data)
-
-    print(out.shape)
-
-
-def test_model():
-    pcs = multi_training.loadPieces("music")
-    data = multi_training.getPieceBatch(pcs)
-
-    model = BiaxialRNNModel([300, 300], [100, 50])
-    print(model)
-
-    out = model(data)
-
-    print(out.shape)
-
-
 def main():
-    test_time_model()
-    test_pitch_model()
-    test_model()
+    pass
 
 
 if __name__ == '__main__':
