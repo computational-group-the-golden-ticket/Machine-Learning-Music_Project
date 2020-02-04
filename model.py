fix: error with gradients and loss function

model.py
@@ -190,8 +190,8 @@ class BiaxialRNNModel(nn.Module):
         #  occupies in the bar and other info.
         input_mat, output_mat = x
 
-        input_mat.requires_grad = True
-        output_mat.requires_grad = True
+        # input_mat.requires_grad = True
+        # output_mat.requires_grad = True
 
         # The last info in time dimension is eliminated, as each data in the
         #   matrix (time, batch, number_notes, features) try to predic the next
@@ -286,6 +286,7 @@ class BiaxialRNNModel(nn.Module):
         return note_state_matrix
 
     def forward(self, x, n=1, training=False):
+        print(training)
         if training:
             return self.train(x)
         else:

multi_training.py
@@ -2,7 +2,7 @@ import os
 import random
 from midi_to_statematrix import *
 from data import *
-
+import numpy
 import signal
 
 import torch
@@ -112,10 +112,11 @@ def train(model, pieces):
     # Calculate NLLLoss, gradients and actualizing parameters; the numbers are
     #   pass to long, this ony works for long. Prediction is passed first,
     #   expected probabilities are passed as second parameter.
-    print(output.shape, output_mat.shape)
-    loss = loss_function(output.long(), output_mat.long())
+    loss = loss_function((output, output_mat))
+    # loss = loss_function(output.long(), output_mat.long())
     loss.backward()
     optimizer.step()
+    print('here')
 
     return loss
 
@@ -146,7 +147,8 @@ def trainPiece(model, pieces, epochs, start=0):
             #  is doing.
             xIpt, xOpt = map(torch.Tensor, getPieceSegment(pieces))
 
-            noteStateMatrixToMidi(numpy.concatenate((numpy.expand_dims(xOpt[0], 0), model(batch_len, 1, xIpt[0])), axis=0), 'output/sample{}'.format(i))
+            noteStateMatrixTomidi(numpy.concatenate((numpy.expand_dims(xOpt[0], 0)
+                , model(batch_len, 1, xIpt[0])), axis=0), 'output/sample{}'.format(i))
 
             # Save the model
             torch.save(model.state_dict(), 'output/params{}.p'.format(i))