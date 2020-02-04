3222330eb3e7c0167ef428da8702349f0f0b479c
Andresfgomez970 <andresf.gomez970@gmail.com>
2020-02-04 10:27:55 -0500

    docs: commenting the code

--- data.py
+++ data.py
@@ -31,7 +31,8 @@ def buildContext(state):
 
 
 def buildBeat(time):
-    return [2 * x - 1 for x in [time % 2, (time // 2) % 2, (time // 4) % 2, (time // 8) % 2]]
+    return [2 * x - 1 for x in [time % 2, (time // 2) % 2, (time // 4) % 2,
+                                (time // 8) % 2]]
 
 
 def noteInputForm(note, state, context, beat):
@@ -46,18 +47,21 @@ def noteInputForm(note, state, context, beat):
 
     part_context = context[pitchclass:] + context[:pitchclass]
 
-    return part_position + part_pitchclass + part_prev_vicinity + part_context + beat + [0]
+    return (part_position + part_pitchclass + part_prev_vicinity /
+            + part_context + beat + [0])
 
 
 def noteStateSingleToInputForm(state, time):
     beat = buildBeat(time)
     context = buildContext(state)
-    return [noteInputForm(note, state, context, beat) for note in range(len(state))]
+    return [noteInputForm(note, state, context, beat)
+            for note in range(len(state))]
 
 
 def noteStateMatrixToInputForm(statematrix):
-    # NOTE: May have to transpose this or transform it in some way to make Theano like it
-    #[startSentinel()] +
-    inputform = [noteStateSingleToInputForm(
-        state, time) for time, state in enumerate(statematrix)]
+    # Each list like the ones in statematrix[:,:,:,0] is transform to a more
+    #  meaningful set of features that gives information about the note
+    #  and its surroundings.
+    inputform = [noteStateSingleToInputForm(state, time)
+                 for time, state in enumerate(statematrix)]
     return inputform
